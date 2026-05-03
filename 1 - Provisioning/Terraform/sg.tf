bc3be5b5dbf38ec38b3c01e0482d02b4a91681e4
askari0102 <48948871+askari0102@users.noreply.github.com>
2026-05-04 03:40:21 +0700

    Update sg.tf

--- 1 - Provisioning/Terraform/sg.tf
+++ 1 - Provisioning/Terraform/sg.tf
@@ -1,64 +1,80 @@
-# 1. SECURITY GROUP GATEWAY 
+# 1. BASE SECURITY GROUPS 
+
 resource "aws_security_group" "sg_gateway" {
   name   = "sg_gateway"
   vpc_id = aws_vpc.main.id
   tags   = { Name = "sg-gateway-public" }
-
-  # HTTP 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HTTPS 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # SSH - Port 6969 open for remote access
-  ingress {
-    from_port   = 6969
-    to_port     = 6969
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Outbound
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
-# 2. SECURITY GROUP PRIVATE 
-# Applied for Staging, Prod K3s, Runner, & Monitoring
 resource "aws_security_group" "sg_private" {
   name   = "sg_private"
   vpc_id = aws_vpc.main.id
   tags   = { Name = "sg-private-internal" }
+}
 
-  # Allow all internal communication within this Security Group
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
+# 2. STANDALONE RULES FOR GATEWAY & PRIVATE
 
-  # Outbound
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# --- Gateway Inbound Rules ---
+# Allow HTTP traffic from anywhere
+resource "aws_security_group_rule" "gw_ingress_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg_gateway.id
+}
+
+# Allow HTTPS traffic from anywhere
+resource "aws_security_group_rule" "gw_ingress_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg_gateway.id
+}
+
+# Allow SSH (Port 6969) for remote access
+resource "aws_security_group_rule" "gw_ingress_ssh" {
+  type              = "ingress"
+  from_port         = 6969
+  to_port           = 6969
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg_gateway.id
+}
+
+# --- Private Inbound Rules ---
+# Allow all internal communication within this Security Group
+resource "aws_security_group_rule" "private_ingress_self" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.sg_private.id
+  security_group_id        = aws_security_group.sg_private.id
+}
+
+# --- Outbound (Egress) Rules ---
+# Allow all outbound traffic from Gateway
+resource "aws_security_group_rule" "gw_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg_gateway.id
+}
+
+# Allow all outbound traffic from Private servers
+resource "aws_security_group_rule" "private_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg_private.id
 }
 
 # 3. CROSS-REFERENCE RULES 
@@ -93,7 +109,27 @@ resource "aws_security_group_rule" "gateway_to_private_grafana" {
   source_security_group_id = aws_security_group.sg_gateway.id
 }
 
-# 3. Allow Gateway to access Frontend App in Staging/K3s
+# 3. Allow Gateway to access Prometheus in Monitoring Server
+resource "aws_security_group_rule" "gateway_to_private_prometheus" {
+  type                     = "ingress"
+  from_port                = 9090
+  to_port                  = 9090
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.sg_private.id
+  source_security_group_id = aws_security_group.sg_gateway.id
+}
+
+# 4. Allow Gateway to access Node Exporter in Monitoring Server
+resource "aws_security_group_rule" "gateway_to_private_node_exporter" {
+  type                     = "ingress"
+  from_port                = 9100
+  to_port                  = 9100
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.sg_private.id
+  source_security_group_id = aws_security_group.sg_gateway.id
+}
+
+# 4. Allow Gateway to access Frontend App in Staging/K3s
 resource "aws_security_group_rule" "gateway_to_private_app_fe" {
   type                     = "ingress"
   from_port                = 80
@@ -103,11 +139,21 @@ resource "aws_security_group_rule" "gateway_to_private_app_fe" {
   source_security_group_id = aws_security_group.sg_gateway.id
 }
 
-# 3. Allow Gateway to access Backend App in Staging/K3s
+# 5. Allow Gateway to access Backend App in Staging/K3s
 resource "aws_security_group_rule" "gateway_to_private_app_be" {
   type                     = "ingress"
-  from_port                = 8080
-  to_port                  = 8080
+  from_port                = 5000
+  to_port                  = 5000
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.sg_private.id
+  source_security_group_id = aws_security_group.sg_gateway.id
+}
+
+# 6. Allow Gateway to access SonarQube in CI/CD Server
+resource "aws_security_group_rule" "gateway_to_private_sonarqube" {
+  type                     = "ingress"
+  from_port                = 9000
+  to_port                  = 9000
   protocol                 = "tcp"
   security_group_id        = aws_security_group.sg_private.id
   source_security_group_id = aws_security_group.sg_gateway.id
